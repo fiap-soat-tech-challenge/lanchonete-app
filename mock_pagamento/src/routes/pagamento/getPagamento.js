aec75b9885eb6f7cd643e5a61ce73213158075ad
Giovanni Tonussi <gtonussi@uolinc.com>
2023-09-04 21:52:05 -0300

    mock: Cria função para mudar status de pagamento

--- mock_pagamento/src/routes/pagamento/getPagamento.js
+++ mock_pagamento/src/routes/pagamento/getPagamento.js
@@ -1,9 +1,7 @@
 export default async (request, response) => {
-    response.send({
-        id: request.params.id,
-        status: 'pagamento confirmado',
-        codigo: 200
-    });
-
-
-}
+  response.send({
+    id: request.params.id,
+    status: 'pagamento confirmado',
+    codigo: 200,
+  });
+};
--- mock_pagamento/src/routes/pagamento/index.js
+++ mock_pagamento/src/routes/pagamento/index.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import postQRCode from './postQRCode.js';
 import getPagamento from './getPagamento.js';
 
-
 var routes = Router();
 
 routes.post('/qrcode', postQRCode);
