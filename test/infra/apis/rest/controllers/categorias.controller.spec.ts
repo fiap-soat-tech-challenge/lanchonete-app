68671c1b076eb163287abe45825e6a7ad2f5ab61
Jonilson Sousa <jonilsonds9@gmail.com>
2023-10-28 22:47:46 -0300

    Adicionando testes

---
+++ test/infra/apis/rest/controllers/categorias.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test } from '@nestjs/testing';
+import { CategoriasController } from '../../../../../src/infra/apis/rest/controllers/categorias.controller';
+import { CategoriaPresenter } from '../../../../../src/infra/apis/rest/presenters/categoria.presenter';
+
+describe('CategoriasController', () => {
+  let categoriasController: CategoriasController;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [CategoriasController],
+    }).compile();
+
+    categoriasController =
+      moduleRef.get<CategoriasController>(CategoriasController);
+  });
+
+  describe('listar', () => {
+    it('deveria retornar uma lista de CategoriaPresenter', async () => {
+      const result: Array<CategoriaPresenter> = [
+        new CategoriaPresenter('LANCHE'),
+        new CategoriaPresenter('ACOMPANHAMENTO'),
+        new CategoriaPresenter('BEBIDA'),
+        new CategoriaPresenter('SOBREMESA'),
+      ];
+
+      jest
+        .spyOn(categoriasController, 'listar')
+        .mockImplementation(() => result);
+
+      expect(categoriasController.listar()).toBe(result);
+    });
+  });
+});
